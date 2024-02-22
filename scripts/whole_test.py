7fb633f6aa1dd98e1e27b1d6f245014f9e067d72
alpylmz <ylmz.alp.e@gmail.com>
2024-02-21 19:49:07 -0500

    update on config

--- scripts/whole_test.py
+++ scripts/whole_test.py
@@ -9,13 +9,18 @@ config_file = """
 
 #include <FixedPoint/fixed_point.hpp> // for OverflowMode
 
+const std::string urdf_filename = std::string("models/panda.urdf");
 
+const bool USE_RENDER = true;
 const int INT_BITS = {int_bits};
 const int FRAC_BITS = {frac_bits};
 
 OverflowMode OVERFLOW_MODE = OverflowMode::CLAMP;
 typedef FixedPoint<INT_BITS, FRAC_BITS> exp_type;
 // typedef double exp_type;
+
+// PD controller uses this position as its target
+exp_type fixed_pos[] = {-0.002493706342403138, -0.703703218059273, 0.11392999851084838, -2.205860629386432, 0.06983090103997125, 1.5706197776794442};
 
 std::string EXPERIMENT_DIRECTORY = "exp/02-20/";
 std::ofstream DATA_FILE;
